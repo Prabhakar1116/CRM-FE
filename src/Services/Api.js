4290e0f011e010fadbba1b2981ca30f467f54628
Prabhakar1116 <duraiprabhakar@gmail.com>
2024-06-25 16:04:08 +0530

    Commit

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update with your backend URL
+  baseURL: 'http://crm-be-4iui.onrender.com/api', // Update with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
